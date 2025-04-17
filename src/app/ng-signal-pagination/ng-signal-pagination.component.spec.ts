2a587f29e9761dd26a67ba2f57ef3134fa13b51a
JP <jp@jp.nl>
2025-04-17 23:24:46 +0200

    Karma/Jasmine out, Vitest in. Set OnPush as the default.

--- src/app/ng-signal-pagination/ng-signal-pagination.component.spec.ts
+++ src/app/ng-signal-pagination/ng-signal-pagination.component.spec.ts
@@ -1,22 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 import { NgSignalPaginationComponent } from './ng-signal-pagination.component';
 
 describe('NgSignalPaginationComponent', () => {
-	let component: NgSignalPaginationComponent;
-	let fixture: ComponentFixture<NgSignalPaginationComponent>;
+	let sut: NgSignalPaginationComponent<any>;
+	let fixture: ComponentFixture<NgSignalPaginationComponent<any>>;
+	let shows: { title: string; rating: number }[];
 
 	beforeEach(async () => {
-		await TestBed.configureTestingModule({
+		shows = [
+			{ title: 'Game of Thrones', rating: 9.5 },
+			{ title: 'Black Mirror', rating: 9.5 },
+			{ title: 'Prison Break', rating: 7 },
+			{ title: 'Lost', rating: 8 },
+			{ title: 'Breaking Bad', rating: 9 },
+			{ title: 'Better Call Saul', rating: 9 },
+			{ title: 'Friends', rating: 8 },
+			{ title: 'Dexter', rating: 7.5 },
+			{ title: 'The Gentlemen', rating: 9 },
+			{ title: 'Ozark', rating: 8 },
+			{ title: 'Alice in Borderland', rating: 7.5 },
+			{ title: 'Squid Game', rating: 7.5 },
+			{ title: 'Now & Again', rating: 8 },
+		];
+		TestBed.configureTestingModule({
 			imports: [NgSignalPaginationComponent],
-		}).compileComponents();
+		});
 
 		fixture = TestBed.createComponent(NgSignalPaginationComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
+		fixture.componentRef.setInput('data', shows);
+		sut = fixture.componentInstance;
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	it('determines the number of pages', () => {
+		expect(4).toBe(14);
 	});
 });
