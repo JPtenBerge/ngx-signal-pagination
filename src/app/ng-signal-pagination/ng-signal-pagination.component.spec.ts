3f6fb03cd28cdbd2205b8344b45a2752bd402572
JP <jp@jp.nl>
2025-04-19 03:04:57 +0200

    Added test for user interaction with page numbers. Formatted a bit. Added coverage.

--- src/app/ng-signal-pagination/ng-signal-pagination.component.spec.ts
+++ src/app/ng-signal-pagination/ng-signal-pagination.component.spec.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it, vi } from 'vitest';
-import { render, RenderResult, screen } from '@testing-library/angular';
+import { fireEvent, render, RenderResult, screen } from '@testing-library/angular';
 
 import { NgSignalPaginationComponent } from './ng-signal-pagination.component';
-import { Show } from '../test-helpers/show.spec';
-import { getShows } from '../test-helpers/shows.data.spec';
+import { Show } from '../test-helpers/show';
+import { getShows } from '../test-helpers/shows.data';
 
 describe('NgSignalPaginationComponent', () => {
 	it('determines the number of pages', async () => {
@@ -13,14 +13,21 @@ describe('NgSignalPaginationComponent', () => {
 	});
 
 	it('offers paginated data', async () => {
-		let result = await renderPagination();
-		expect(result.fixture.componentInstance.pageData()).toEqual(getShows().slice(0, 5));
+		let { fixture } = await renderPagination();
+		expect(fixture.componentInstance.pageData()).toEqual(getShows().slice(0, 5));
 	});
 
-	it('responds to page changes', async () => {
-		let result = await renderPagination();
-		result.fixture.componentInstance.currentPage.set(2);
-		expect(result.fixture.componentInstance.pageData()).toEqual(getShows().slice(5, 10));
+	it('responds to page changes through its signal', async () => {
+		let { fixture } = await renderPagination();
+		fixture.componentInstance.currentPage.set(2);
+		expect(fixture.componentInstance.pageData()).toEqual(getShows().slice(5, 10));
+	});
+
+	it('responds to page changes when the user clicks the page numbers ', async () => {
+		let { fixture } = await renderPagination();
+		let secondPageElem = screen.getByRole('list').children[1];
+		fireEvent(secondPageElem, new MouseEvent('click'));
+		expect(fixture.componentInstance.pageData()).toEqual(getShows().slice(5, 10));
 	});
 
 	const renderPagination = async (nrOfItemsPerPage = 5): Promise<RenderResult<NgSignalPaginationComponent<Show>>> => {
--- vite.config.mts
+++ vite.config.mts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
 			setupFiles: ['src/test-setup.ts'],
 			include: ['**/*.spec.ts'],
 			reporters: ['default'],
+			coverage: { provider: 'v8' },
 		},
 		define: {
 			'import.meta.vitest': mode !== 'production',
